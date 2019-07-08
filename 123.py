09280593efce884058404d884000f0050f674062
Evgenyi Yakimchuk <Kum4yk@gmail.com>
2019-07-08 14:48:17 +0600

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,19 +1,4 @@
-roots = input().split()
-affixes = input().split()
-word = input()
-is_word = False
-for root in roots:
-    if word.startswith(root):
-        for suffix in affixes:
-            if word == root + suffix:
-                is_word = True
-                break
-    elif word.endswith(root):
-        for prefix in affixes:
-            if word == prefix + root:
-                is_word = True
-                break
-if is_word:
-    print("It is a word")
-else:
-    print("It is not a word")
+a = [(x, y) for x,y in range(10)]
+
+print(map(lambda x, y: x ** 2, a))
+
