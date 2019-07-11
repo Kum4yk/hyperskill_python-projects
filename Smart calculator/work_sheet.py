8adaf2fd03c096a06d6e300aa9d2430d294496b3
Evgenyi Yakimchuk <Kum4yk@gmail.com>
2019-07-11 17:50:46 +0600

    Update work_sheet.py

--- Smart calculator/work_sheet.py
+++ Smart calculator/work_sheet.py
@@ -1,30 +1,51 @@
 class SmartCalculator:
-    @staticmethod
-    def input_working():
-        stop = "/exit"
-        while True:
-            values = input().rstrip()
-            if values == stop:
-                print("Bye!")
-                break
-            elif values == "/help":
-                print("The program calculates the sum of numbers")
-            elif values == '':
-                continue
-            print(SmartCalculator.string_handling(values))
+    operations = {'+', '-', '='}
+
+    def __init__(self):
+        self.variables = dict()
 
     @staticmethod
+    def main():
+        while True:
+            values = input().rstrip()
+            if values == '':
+                continue
+            if values[0] == "/":
+                if values == '/help':
+                    print("The program calculates the sum of numbers")
+                elif values == "/exit":
+                    print("Bye!")
+                    break
+                else:
+                    print("Unknown command")
+                continue
+            print(SmartCalculator.string_handling(values))
+    ''.count()
+    @staticmethod
     def string_handling(input_string):
+        wait_number = True
         sign = 0
         result = 0
         for number in input_string.split():
-            if number.isnumeric() or number[1:].isnumeric() or number[:-1].isnumeric():
-                result += int(number) * (1 if sign % 2 == 0 else -1)
+            if wait_number:
+                try:
+                    result += int(number) * (1 if sign % 2 == 0 else -1)
+                except ValueError:
+                    return "Invalid expression"
                 sign = 0
-            elif number[0] == '-':
-                sign += len(number)
+                wait_number = False
+            else:
+                wait_number = True
+                if number[0] in SmartCalculator.operations:
+                    if number[0] == '-':
+                        sign += len(number)
+                else:
+                    return "Invalid expression"
         return result
 
 
 if __name__ == "__main__":
-    SmartCalculator.input_working()
+    new = SmartCalculator()
+    new.main()
+
+
